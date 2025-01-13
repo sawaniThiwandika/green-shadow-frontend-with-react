99724e02935401a3c552fac8b587ef2588d70c8c
User <thiwandika.whs02@gmail.com>
2025-01-13 13:45:00 +0530

    set the modal to add new staff

--- src/pages/StaffPage.tsx
+++ src/pages/StaffPage.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { TableComponent } from '../components/TableComponent.tsx';
 import { FaSearch} from 'react-icons/fa';
 import {BiPlus} from "react-icons/bi";
+import {ModalComponent} from "../components/ModalComponent.tsx";
+
+
+
 
 export function StaffPage() {
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    function handleAddButton() {
+      setIsModalOpen(true);
+    }
 
     const dataSource = [
         {
@@ -56,6 +65,16 @@ export function StaffPage() {
         },
     ];
 
+    const showModal = () => setIsModalOpen(true);
+    const closeModal = () => setIsModalOpen(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const formObject = Object.fromEntries(formData.entries());
+        console.log("Form Data:", formObject);
+        closeModal();
+    };
 
 
 
@@ -78,7 +97,7 @@ export function StaffPage() {
                 </div>
 
                 <div className="ml-4 mr-10">
-                    <button
+                    <button onClick={handleAddButton}
                         type="button"
                         className="flex items-center text-white bg-green-700 rounded-md px-4 py-2 hover:border-green-950 hover:bg-white hover:text-black">
                         Add New
@@ -88,6 +107,166 @@ export function StaffPage() {
             </div>
 
             <TableComponent dataSource={dataSource} columns={columns} />
+
+            <ModalComponent
+                title="Add New Staff"
+                isOpen={isModalOpen}
+                onClose={closeModal}
+            >
+                <form onSubmit={handleSubmit}>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 mb-1">Staff ID</label>
+                        <input
+                            type="text"
+                            name="staffId"
+                            className="w-full border border-gray-300 rounded-md px-2 py-1"
+                            required
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 mb-1">First Name</label>
+                        <input
+                            type="text"
+                            name="fname"
+                            className="w-full border border-gray-300 rounded-md px-2 py-1"
+                            required
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 mb-1">Last Name</label>
+                        <input
+                            type="text"
+                            name="lname"
+                            className="w-full border border-gray-300 rounded-md px-2 py-1"
+                            required
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 mb-1">designation</label>
+                        <input
+                            type="text"
+                            name="designation"
+                            className="w-full border border-gray-300 rounded-md px-2 py-1"
+                            required
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 mb-1">Gender</label>
+                        <select
+                            name="gender"
+                            className="w-full border border-gray-300 rounded-md px-2 py-1"
+                            required
+                        >
+                            <option value="">Select Gender</option>
+                            <option value="Male">Male</option>
+                            <option value="Female">Female</option>
+                            <option value="Other">Other</option>
+                        </select>
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 mb-1">Contact No.</label>
+                        <input
+                            type="text"
+                            name="contactNo"
+                            className="w-full border border-gray-300 rounded-md px-2 py-1"
+                            required
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 mb-1">Email</label>
+                        <input
+                            type="email"
+                            name="email"
+                            className="w-full border border-gray-300 rounded-md px-2 py-1"
+                            required
+                        />
+                    </div>
+
+                    <div className="mb-4">
+                        <label className="block text-gray-700 mb-1">Joined Date</label>
+                        <input
+                            type="date"
+                            name="joinedDate"
+                            className="w-full border border-gray-300 rounded-md px-2 py-1"
+                            required
+                        />
+                    </div>
+
+                    <div className="mb-4">
+                        <label className="block text-gray-700 mb-1">Date of Birth</label>
+                        <input
+                            type="date"
+                            name="dob"
+                            className="w-full border border-gray-300 rounded-md px-2 py-1"
+                            required
+                        />
+                    </div>
+
+                    <div className="mb-4">
+                        <label className="block text-gray-700 mb-1">Address</label>
+                        <input
+                            type="email"
+                            name="address"
+                            className="w-full border border-gray-300 rounded-md px-2 py-1"
+                            required
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 mb-1" htmlFor="role">Role</label>
+                        <select
+                            id="role"
+                            name="role"
+                            className="w-full border border-gray-300 rounded-md px-2 py-1"
+                            required
+                        >
+                            <option value="">Select role</option>
+                            <option value="Manager">Manager</option>
+                            <option value="Worker">Worker</option>
+                        </select>
+                    </div>
+
+                    <div className="mb-4">
+                        <label className="block text-gray-700 mb-1" htmlFor="fields">Allocated Fields</label>
+                        <input
+                            type="text"
+                            id="fields"
+                            name="fields"
+                            className="w-full border border-gray-300 rounded-md px-2 py-1"
+                            placeholder="Enter fields or N/A"
+                            required
+                        />
+                    </div>
+
+                    <div className="mb-4">
+                        <label className="block text-gray-700 mb-1" htmlFor="vehicle">Vehicle</label>
+                        <input
+                            type="text"
+                            id="vehicle"
+                            name="vehicle"
+                            className="w-full border border-gray-300 rounded-md px-2 py-1"
+                            placeholder="Enter vehicle details"
+                            required
+                        />
+                    </div>
+
+
+                    {/* <div className="flex justify-end">
+                        <button
+                            type="button"
+                            className="px-4 py-2 mr-2 border border-gray-300 rounded-md text-gray-700"
+                            onClick={closeModal}
+                        >
+                            Cancel
+                        </button>
+                        <button
+                            type="submit"
+                            className="px-4 py-2 bg-green-700 text-white rounded-md hover:bg-green-800"
+                        >
+                            Submit
+                        </button>
+                    </div>*/}
+                </form>
+            </ModalComponent>
         </div>
     );
 
