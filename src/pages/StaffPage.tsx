d0b6d34f0d2e4fa8d1e0b7cfd16e6428084c10b6
User <thiwandika.whs02@gmail.com>
2025-01-14 18:31:45 +0530

    responsive staff page

--- src/pages/StaffPage.tsx
+++ src/pages/StaffPage.tsx
@@ -1,17 +1,14 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { TableComponent } from '../components/TableComponent.tsx';
-import { FaSearch} from 'react-icons/fa';
-import {BiPlus} from "react-icons/bi";
-import {ModalComponent} from "../components/ModalComponent.tsx";
-
-
-
+import { FaSearch } from 'react-icons/fa';
+import { BiPlus } from "react-icons/bi";
+import { ModalComponent } from "../components/ModalComponent.tsx";
 
 export function StaffPage() {
 
     const [isModalOpen, setIsModalOpen] = useState(false);
     function handleAddButton() {
-      setIsModalOpen(true);
+        setIsModalOpen(true);
     }
 
     const dataSource = [
@@ -30,7 +27,6 @@ export function StaffPage() {
             email: 'sawani.wh@gmail.com',
         },
     ];
-
 
     const columns = [
         {
@@ -56,12 +52,10 @@ export function StaffPage() {
         {
             title: 'Update',
             key: 'update',
-
         },
         {
             title: 'Delete',
             key: 'delete',
-
         },
     ];
 
@@ -76,16 +70,14 @@ export function StaffPage() {
         closeModal();
     };
 
-
-
     return (
         <div className="container mx-auto p-6">
-            <div className="flex justify-between items-center mb-4">
+            <div className="flex flex-col sm:flex-row justify-between items-center mb-4">
                 <h1 className="text-3xl font-bold text-gray-800 mt-3">Staff Page</h1>
             </div>
 
-            <div className="flex items-center justify-between mb-4">
-                <div className="flex-grow flex justify-center">
+            <div className="flex flex-col sm:flex-row items-center justify-between mb-4">
+                <div className="flex-grow flex justify-center mb-4 sm:mb-0">
                     <div className="flex items-center border border-gray-300 rounded-md p-2 w-full max-w-md">
                         <FaSearch className="text-gray-600 mr-2" />
                         <input
@@ -96,10 +88,10 @@ export function StaffPage() {
                     </div>
                 </div>
 
-                <div className="ml-4 mr-10">
+                <div className="ml-4 sm:ml-10">
                     <button onClick={handleAddButton}
-                        type="button"
-                        className="flex items-center text-white bg-green-700 rounded-md px-4 py-2 hover:border-green-950 hover:bg-white hover:text-black">
+                            type="button"
+                            className="flex items-center text-white bg-green-700 rounded-md px-4 py-2 hover:border-green-950 hover:bg-white hover:text-black">
                         Add New
                         <BiPlus className="ml-3" />
                     </button>
@@ -142,7 +134,7 @@ export function StaffPage() {
                         />
                     </div>
                     <div className="mb-4">
-                        <label className="block text-gray-700 mb-1">designation</label>
+                        <label className="block text-gray-700 mb-1">Designation</label>
                         <input
                             type="text"
                             name="designation"
@@ -205,12 +197,13 @@ export function StaffPage() {
                     <div className="mb-4">
                         <label className="block text-gray-700 mb-1">Address</label>
                         <input
-                            type="email"
+                            type="text"
                             name="address"
                             className="w-full border border-gray-300 rounded-md px-2 py-1"
                             required
                         />
                     </div>
+
                     <div className="mb-4">
                         <label className="block text-gray-700 mb-1" htmlFor="role">Role</label>
                         <select
@@ -249,26 +242,8 @@ export function StaffPage() {
                         />
                     </div>
 
-
-                    {/* <div className="flex justify-end">
-                        <button
-                            type="button"
-                            className="px-4 py-2 mr-2 border border-gray-300 rounded-md text-gray-700"
-                            onClick={closeModal}
-                        >
-                            Cancel
-                        </button>
-                        <button
-                            type="submit"
-                            className="px-4 py-2 bg-green-700 text-white rounded-md hover:bg-green-800"
-                        >
-                            Submit
-                        </button>
-                    </div>*/}
                 </form>
             </ModalComponent>
         </div>
     );
-
 }
-
