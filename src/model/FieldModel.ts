e15eac50f1b41176900b6e267d452b82bca6d2d0
Sawani Thiwandika <thiwandika.whs02@gmail.com>
2025-02-11 08:40:46 +0530

    update the fieldModel - change crop attribute name to cropCode

--- src/model/FieldModel.ts
+++ src/model/FieldModel.ts
@@ -6,20 +6,22 @@ export class FieldModel {
     fieldLocation: string;
     fieldSize: string;
     fieldImage1: string;
-    crop: string;
+    cropCode: string;
     staff: string[];
     equipment: string[];
+    log:string[]
 
 
-    constructor(fieldCode: string, fieldName: string, fieldLocation: string, fieldSize: string, fieldImage1: string,  crop: string, staff: string[] , equipment:string[]) {
+    constructor(fieldCode: string, fieldName: string, fieldLocation: string, fieldSize: string, fieldImage1: string,  crop: string, staff: string[] , equipment:string[],log:string[]) {
         this.fieldCode = fieldCode;
         this.fieldName = fieldName;
         this.fieldLocation = fieldLocation;
         this.fieldSize = fieldSize;
         this.fieldImage1 = fieldImage1;
-        this.crop = crop;
+        this.cropCode = crop;
         this.staff = staff;
         this.equipment=equipment;
+        this.log=log;
 
     }
 
