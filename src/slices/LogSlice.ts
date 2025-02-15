3a3f5cc6727056e80889ba4ce937d4c05e0e9e05
Sawani Thiwandika <thiwandika.whs02@gmail.com>
2025-02-15 23:22:51 +0530

    call the add log endpoint send data to backend

--- src/slices/LogSlice.ts
+++ src/slices/LogSlice.ts
@@ -1,9 +1,41 @@
-import { createSlice } from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
 import {LogModel} from "../model/LogModel";
+import axios from "axios";
 
 const initialState = {
     logs: [] as LogModel[],
 };
+const api=axios.create({
+    baseURL:'http://localhost:3000/log',
+});
+
+export const saveLog = createAsyncThunk('logSlice/saveLog',
+    async (log :LogModel) => {
+        try {
+
+            const formData = new FormData();
+            formData.append("logCode", log.logCode);
+            formData.append("logDate", log.logDate);
+            formData.append("logDetails",log.logDetails);
+            formData.append("relevantCrops", log.relevantCrops);
+            formData.append("relevantFields", log.relevantFields);
+            formData.append("observedImage",log.observedImage);
+            formData.append("relevantStaff", JSON.stringify(log.relevantStaff));
+
+            const response = await api.post('/add', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+            });
+            return response.data;
+
+        } catch (error) {
+            console.error("Error saving log:", error);
+            throw error;
+        }
+    });
+
+
 
 const logSlice = createSlice({
     name: "logSlice",
@@ -27,6 +59,22 @@ const logSlice = createSlice({
             );
         },
     },
+    extraReducers:(builder) => {
+        builder
+            .addCase(saveLog.pending, (state, action) => {
+                console.log("Pending");
+            })
+
+            .addCase(saveLog.fulfilled, (state, action) => {
+                console.log("Fulfilled with data:", action.payload);
+                state.logs.push(action.payload);
+            })
+            .addCase(saveLog.rejected, (state, action) => {
+                console.log("Rejected");
+            })
+
+    }
+
 });
 
 export const { addLog, updateLog, deleteLog } = logSlice.actions;
